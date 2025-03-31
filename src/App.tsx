b273ff75ae6fc526072518c6b8c1d2fe0ce5fad4
Luka <battelerx@gmail.com>
2025-03-31 22:33:09 +0400

    Initialization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import OSVideoPlayer from "./OS_PLAYER/OSVideoPlayer";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <OSVideoPlayer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/OS_PLAYER/OSVideoPlayer.tsx
@@ -0,0 +1,7 @@
+export default function OSVideoPlayer() {
+  return (
+    <div className="h-[600px] aspect-video bg-black">
+      <video src="video/mp4/video.mp4"></video>
+    </div>
+  );
+}
